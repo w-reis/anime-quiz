9157faf7b6d8d660916a8c27ce846327f69803e7
Wellinton Reis <wellinton_fabricio@hotmail.com>
2021-01-26 16:47:52 -0300

    :sparkles: create global styles file

---
+++ src/styles/global.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+import db from '../../db.json';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`
+const theme = db.theme;
+
+export function Theme({children}){
+  return (
+    <ThemeProvider theme={theme}>
+      {children}
+    </ThemeProvider>
+  );  
+}
