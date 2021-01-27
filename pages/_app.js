f09815da3c6f5f7a42f682ab4afec629ebbc536b
Wellinton Reis <wellinton_fabricio@hotmail.com>
2021-01-26 23:13:26 -0300

    :sparkles: import Lato font

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import { GlobalStyle, Theme } from '../src/styles/global'
-
+import Head from 'next/head'
 export default function App({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet"/>
+    </Head>
       <Theme>
         <GlobalStyle />
         <Component {...pageProps} />
       </Theme>
+     </> 
   )
 }
