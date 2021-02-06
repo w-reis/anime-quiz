7842a67e37e6929c4b2a321a5687b4ecedae185a
Wellinton Reis <wellinton_fabricio@hotmail.com>
2021-02-06 19:37:21 -0300

    :sparkles: importing container

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-import styled from 'styled-components';
 import Widget from '../src/components/Widget';
+import QuizContainer from '../src/components/QuizContainer';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizInput from '../src/components/QuizInput';
@@ -13,17 +13,6 @@ import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 
 import db from '../db.json';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const router = useRouter();
